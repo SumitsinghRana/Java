c561d94d579b1b838aff2613c15c5a0ffdc7cbab
SumitsinghRana <149795791+SumitsinghRana@users.noreply.github.com>
2024-01-23 22:32:17 +0530

    java

---
+++ pattern3.java
@@ -0,0 +1,15 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 5 - i; k++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
