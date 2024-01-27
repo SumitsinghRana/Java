4f9e3b09bf2a6c1ea88e3d5d2e323a7c721aa266
SumitsinghRana <149795791+SumitsinghRana@users.noreply.github.com>
2024-01-27 20:07:00 +0530

    java

---
+++ pattern9.java
@@ -0,0 +1,18 @@
+public class pattern9 {
+
+    public static void main(String args[]) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop to print star
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
